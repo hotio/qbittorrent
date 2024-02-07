e02555023b8cc343eedd7bb04bda2a850a2c0568
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-02-07 09:20:42 +0100

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-
-nightwalker_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/CallMeBruce/nightwalker/commits/main" | jq -r .sha)
-[[ -z ${nightwalker_version} ]] && exit 0
-vuetorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/vuetorrent/vuetorrent/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${vuetorrent_version} ]] && exit 0
-full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static-legacy/releases/latest/download/dependency-version.json" | jq -r '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"')
-version=$(echo "${full_version}" | sed -e "s/release-//g" -e "s/_.*//g")
-[[ -z ${version} ]] && exit 0
-old_version=$(jq -r '.version' < VERSION.json)
-changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/qbittorrent/qbittorrent/compare/release-${old_version}...release-${version}"
-version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .full_version = "'"${full_version}"'" | .vuetorrent_version = "'"${vuetorrent_version}"'" | .nightwalker_version = "'"${nightwalker_version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
+set -e
+nightwalker_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/CallMeBruce/nightwalker/commits/main" | jq -re .sha)
+vuetorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/vuetorrent/vuetorrent/releases/latest" | jq -re .tag_name)
+full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static-legacy/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"')
+version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
+json=$(cat VERSION.json)
+jq --sort-keys \
+    --arg version "${version//v/}" \
+    --arg full_version "${full_version}" \
+    --arg vuetorrent_version "${vuetorrent_version//v/}" \
+    --arg nightwalker_version "${nightwalker_version//v/}" \
+    '.version = $version | .full_version = $full_version | .vuetorrent_version = $vuetorrent_version | .nightwalker_version = $nightwalker_version' <<< "${json}" | tee VERSION.json
