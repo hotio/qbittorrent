60211e7e30aa8f2fa5640763438c53f0933151ce
mrhotio <26902309+mrhotio@users.noreply.github.com>
2024-04-26 07:06:16 +0200

    [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -3,6 +3,8 @@ nightwalker_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://a
 vuetorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/vuetorrent/vuetorrent/releases/latest" | jq -re .tag_name) || exit 1
 full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"') || exit 1
 version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
+[[ -z ${version} ]] && exit 0
+[[ ${version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
