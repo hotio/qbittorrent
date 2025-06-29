d6ae4078c8105a3525c3031407f4745f0642a73d
mrhotio <26902309+mrhotio@users.noreply.github.com>
2025-06-29 11:00:22 +0200

    take into account upstream revision

--- linux-amd64.Dockerfile
+++ linux-amd64.Dockerfile
@@ -9,6 +9,7 @@ ENV IMAGE_STATS=${IMAGE_STATS} WEBUI_PORTS="8080/tcp,8080/udp"
 RUN ln -s "${CONFIG_DIR}" "${APP_DIR}/qBittorrent"
 
 ARG FULL_VERSION
+ARG BUILD_REVISION
 RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static-legacy/releases/download/${FULL_VERSION}/x86_64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox" && \
     chmod 755 "${APP_DIR}/qbittorrent-nox"
 
--- update-versions.sh
+++ update-versions.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static-legacy/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"') || exit 1
 version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
+build_revision=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static-legacy/releases/download/${full_version}/dependency-version.json" | jq -re '.revision') || exit 1
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
     --arg full_version "${full_version}" \
-    '.version = $version | .full_version = $full_version' <<< "${json}" | tee VERSION.json
+    --arg build_revision "${build_revision}" \
+    '.version = $version | .full_version = $full_version | .build_revision = $build_revision' <<< "${json}" | tee VERSION.json
