e8dada05befebf51d73669a05b09ef046c6f69e6
mrhotio <26902309+mrhotio@users.noreply.github.com>
2025-06-29 12:50:44 +0200

    bundle both libtorrent v1 and v2 versions

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -4,13 +4,17 @@ ARG UPSTREAM_DIGEST_ARM64
 FROM ${UPSTREAM_IMAGE}@${UPSTREAM_DIGEST_ARM64}
 EXPOSE 8080
 ARG IMAGE_STATS
-ENV IMAGE_STATS=${IMAGE_STATS} WEBUI_PORTS="8080/tcp,8080/udp"
+ENV IMAGE_STATS=${IMAGE_STATS} WEBUI_PORTS="8080/tcp,8080/udp" LIBTORRENT="v1"
 
 RUN ln -s "${CONFIG_DIR}" "${APP_DIR}/qBittorrent"
 
-ARG FULL_VERSION
-ARG BUILD_REVISION
-RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION}/aarch64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox" && \
-    chmod 755 "${APP_DIR}/qbittorrent-nox"
+ARG FULL_VERSION_LIB1
+ARG BUILD_REVISION_LIB1
+ARG FULL_VERSION_LIB2
+ARG BUILD_REVISION_LIB2
+RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION_LIB1}/aarch64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox-lib1" && \
+    chmod 755 "${APP_DIR}/qbittorrent-nox-lib1" && \
+    curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION_LIB2}/aarch64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox-lib2" && \
+    chmod 755 "${APP_DIR}/qbittorrent-nox-lib2"
 
 COPY root/ /
--- update-versions.sh
+++ update-versions.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"') || exit 1
-version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
-build_revision=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version}/dependency-version.json" | jq -re '.revision') || exit 1
+full_version_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"') || exit 1
+build_revision_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib1}/dependency-version.json" | jq -re '.revision') || exit 1
+full_version_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
+build_revision_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib2}/dependency-version.json" | jq -re '.revision') || exit 1
+version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version_lib1}")
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
-    --arg full_version "${full_version}" \
-    --arg build_revision "${build_revision}" \
-    '.version = $version | .full_version = $full_version | .build_revision = $build_revision' <<< "${json}" | tee VERSION.json
+    --arg full_version_lib1 "${full_version_lib1}" \
+    --arg build_revision_lib1 "${build_revision_lib1}" \
+    --arg full_version_lib2 "${full_version_lib2}" \
+    --arg build_revision_lib2 "${build_revision_lib2}" \
+    '.version = $version | .full_version_lib1 = $full_version_lib1 | .build_revision_lib1 = $build_revision_lib1 | .full_version_lib2 = $full_version_lib2 | .build_revision_lib2 = $build_revision_lib2' <<< "${json}" | tee VERSION.json
