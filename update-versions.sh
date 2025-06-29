73eba3888147a734af3a19fd376edbe0c4d5d875
mrhotio <26902309+mrhotio@users.noreply.github.com>
2025-06-29 10:42:48 +0200

    take into account upstream revision

--- update-versions.sh
+++ update-versions.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"') || exit 1
+json=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json") || exit 1
+full_version=$(jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"' <<< "${json}" ) || exit 1
+build_revision=$(jq -re '.revision' <<< "${json}" ) || exit 1
 version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
 [[ -z ${version} ]] && exit 0
 [[ ${version} == null ]] && exit 0
@@ -7,4 +9,5 @@ json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${version//v/}" \
     --arg full_version "${full_version}" \
-    '.version = $version | .full_version = $full_version' <<< "${json}" | tee VERSION.json
+    --arg build_revision "${build_revision}" \
+    '.version = $version | .full_version = $full_version | .build_revision = $build_revision' <<< "${json}" | tee VERSION.json
